aae4c1c474ce2c83347201be02c7d0623a7eba18
Anushka Hingankar <anuhingankar9922@gmail.com>
2026-07-22 14:56:33 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,99 +2,103 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load("house_price_model.pkl")
+model=joblib.load("heart_model.pkl")
+scaler=joblib.load("scaler.pkl")
+columns=joblib.load("columns.pkl")
 
-city_encoder = joblib.load("city_encoder.pkl")
-state_encoder = joblib.load("state_encoder.pkl")
-country_encoder = joblib.load("country_encoder.pkl")
-
-st.title("House Price Prediction")
-
-st.write("Enter House Details")
-
-bedrooms = st.number_input("Bedrooms", 1, 10, 3)
-
-bathrooms = st.number_input("Bathrooms", 1.0, 10.0, 2.0)
-
-sqft_living = st.number_input("Sqft Living", 500, 15000, 2000)
-
-sqft_lot = st.number_input("Sqft Lot", 1000, 1000000, 5000)
-
-floors = st.number_input("Floors", 1.0, 5.0, 1.0)
-
-waterfront = st.selectbox("Waterfront", [0,1])
-
-view = st.selectbox("View", [0,1,2,3,4])
-
-condition = st.slider("Condition",1,5,3)
-
-sqft_above = st.number_input("Sqft Above",500,10000,1500)
-
-sqft_basement = st.number_input("Sqft Basement",0,5000,500)
-
-yr_built = st.number_input("Year Built",1900,2024,2000)
-
-yr_renovated = st.number_input("Year Renovated",0,2024,0)
-
-city = st.selectbox(
-    "City",
-    city_encoder.classes_
+st.set_page_config(
+    page_title="Heart Disease Prediction",
+    layout="centered"
 )
 
-statezip = st.selectbox(
-    "State Zip",
-    state_encoder.classes_
+st.title("Heart Disease Prediction")
+st.write("Enter patient Details........")
+
+age=st.number_input("Age",min_value=1,max_value=120,value=40)
+
+sex=st.selectbox(
+    "sex",
+    ["M","F"]
 )
 
-country = st.selectbox(
-    "Country",
-    country_encoder.classes_
+chestpain = st.selectbox(
+    "Chest Pain Type",
+    ["ATA","NAP","ASY","TA"]
 )
 
-if st.button("Predict Price"):
+bp=st.number_input(
+    "Resting Blood Pressure",
+    min_value=50,
+    max_value=250,
+    value=120
+)
 
-    city = city_encoder.transform([city])[0]
-    statezip = state_encoder.transform([statezip])[0]
-    country = country_encoder.transform([country])[0]
+chol=st.number_input(
+    "Cholesterol",
+    min_value=0,
+    max_value=700,
+    value=200
+)
 
-    data = pd.DataFrame([[
+fbs=st.selectbox(
+    "Fasting Blood Suger",
+    [0,1]
+)
 
-        bedrooms,
-        bathrooms,
-        sqft_living,
-        sqft_lot,
-        floors,
-        waterfront,
-        view,
-        condition,
-        sqft_above,
-        sqft_basement,
-        yr_built,
-        yr_renovated,
-        city,
-        statezip,
-        country
+ecg=st.selectbox(
+    "Resting ECG",
+    ["Normal","ST","LVH"]
+)
 
-    ]], columns=[
+maxhr=st.number_input(
+    "Maximum Heart Rate",
+    min_value=50,
+    max_value=250,
+    value=150
+)
 
-        'bedrooms',
-        'bathrooms',
-        'sqft_living',
-        'sqft_lot',
-        'floors',
-        'waterfront',
-        'view',
-        'condition',
-        'sqft_above',
-        'sqft_basement',
-        'yr_built',
-        'yr_renovated',
-        'city',
-        'statezip',
-        'country'
+angina=st.selectbox(
+    "Exercise Angina",
+    ["N","Y"]
+)
 
-    ])
+oldpeak=st.number_input(
+    "Old Peak",
+    value=1.0
+)
 
-    prediction = model.predict(data)
+slope=st.selectbox(
+    "ST Slope",
+    ["Up","Flat","Down"]
+)
 
-    st.success(f"Predicted House Price: ${prediction[0]:,.2f}")
+if st.button("Predict"):
+
+    sample = {
+        "Age": age,
+        "Sex": sex,
+        "ChestPainType": chestpain,
+        "RestingBP": bp,
+        "Cholesterol": chol,
+        "FastingBS": fbs,
+        "RestingECG": ecg,
+        "MaxHR": maxhr,
+        "ExerciseAngina": angina,
+        "Oldpeak": oldpeak,
+        "ST_Slope": slope
+    }
+
+    sample_df = pd.DataFrame([sample])
+
+    sample_df = pd.get_dummies(sample_df, drop_first=True)
+
+    sample_df = sample_df.reindex(columns=columns, fill_value=0)
+
+    sample_scaled = scaler.transform(sample_df)
+
+    prediction = model.predict(sample_scaled)
+
+    if prediction[0] == 1:
+        st.error("Heart Disease: Yes")
+    else:
+        st.success("Heart Disease: No")
